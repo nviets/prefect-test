87010e4373105f93b8250ce6e8b9bbfb3110b738
Nathan Viets <nathan.g.viets@gmail.com>
2023-10-11 20:23:45 -0500

    first commit

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  # this is a riff on the standard flake template for a python project
+  # https://github.com/NixOS/templates/blob/master/python/flake.nix
+
+  # Prefect is on a branch of my fork of nixpkgs
+  inputs.nixpkgs.url = "github:nviets/nixpkgs/0c7e26ee5b80aa99c3aaff5aa98a491b2e93c570";
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      pkgs = forAllSystems (system: nixpkgs.legacyPackages.${system});
+    in
+    {
+      packages = forAllSystems (system: {
+        default = pkgs.${system}.python3Packages.prefect;
+      });
+
+      devShells = forAllSystems (system: {
+        default = pkgs.${system}.mkShellNoCC {
+          packages = with pkgs.${system}; [
+            # add your packages here, then run "nix develop"
+            python3Packages.prefect
+          ];
+        };
+      });
+    };
+}
